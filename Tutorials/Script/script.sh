33b65e03921574e3b6687492ca2e175d245a9c2a
Matteo Biagetti <matteo.1biagetti@gmail.com>
2023-10-15 22:49:32 +0200

    edits

--- Tutorials/Script/script.sh
+++ Tutorials/Script/script.sh
@@ -5,7 +5,7 @@
 KBIN=1.
 KCENTER=1.
 
-#python test.py --kbin $KBIN --kcenter $KCENTER 
+python test.py --kbin $KBIN --kcenter $KCENTER 
 
 # change grid and LOS 
 #(note that change in LOS is not compatible with Sancho dataset, here just to show a test case)
@@ -15,7 +15,7 @@ LOSx=0
 LOSy=2
 LOSz=1
 
-#python test.py --grid $GRID --los ${LOSx} ${LOSy} ${LOSz}
+python test.py --grid $GRID --los ${LOSx} ${LOSy} ${LOSz}
 
 # How to put to false a parameter in the config file: 
 #Ex: measurements with no interlacing
